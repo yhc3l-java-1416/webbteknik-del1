856628868fc62055526cf3901d274c6019940564
Tom Blackmore <tablackmore@gmail.com>
2014-10-01 08:54:56 +0200

    added removed file

---
+++ lectures/week6_dom/day2/exercise/keypress/script.js
@@ -0,0 +1,8 @@
+// 1) Wait until the dom content is loaded using an event listener on the window object.
+
+// 2) Add an event listener to the window object that listens to all key strokes
+// 3) Write a switch statement in the above function that changes the text of the '
+// paragraph depending on the arrow keypressed.
+//  hint you need to look at the keyCode property 
+// 4) In the switch statement change the text of the paragraph to match the arrow key pressed
+// i.e. Arrow up "I'm moving up" etc.
