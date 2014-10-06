c7863bbb2f25c2e8983ab29382475c6cc9320631
Tom Blackmore <tablackmore@gmail.com>
2014-10-06 09:12:51 +0200

    added jquery day 1

---
+++ lectures/week7_jQuery/day1/exercise/shoppingList/script.js
@@ -0,0 +1,5 @@
+'use strict';
+// 1) Listen to the click event on the span with the id of button
+// 2) When you click on it retrieve the value from the input element and 
+// add it to the unordered list as a "li" element
+// 3) Can you add a remove button beside each list item that will remove it from the list if clicked?
