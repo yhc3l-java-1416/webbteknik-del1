7938d465c688266424d56a57299e64835fa99f6a
Tom Blackmore <tablackmore@gmail.com>
2014-10-08 10:15:47 +0200

    added completed exercises for day 2

--- lectures/week7_jQuery/day2/exercise-completed/blocket/js/script.js
+++ lectures/week7_jQuery/day2/exercise-completed/blocket/js/script.js
@@ -1,5 +1,38 @@
 'use strict';
 console.log('script');
-// Can you calculate the price of all items on the page?
-// Can you return the title (link name) of all the posts with multiple images.
-// Can you calculate the price of all animals on the page.
+
+var sum = 0;
+$(function () {
+	// Can you calculate the price of all items on the page?
+	$('.list_price').each(function () {
+		var text = $(this).text();
+		text = text.replace(' ', '');
+		var price = parseInt(text, 10);
+		//console.log(price);
+		var isANumber = (!isNaN(price));
+		if (isANumber) {
+			sum = sum + price;
+			console.log(sum);
+		}
+	});
+
+	// Can you return the title (link name) of all the posts with multiple images.
+
+	$('.has_multiple_images').each(function () {
+		console.log($(this).parent().find('.item_link').text());
+	});
+
+	// Can you calculate the price of all animals on the page.
+
+	$('a[title*="Djur"]').parent().parent().find('.list_price').each(function () {
+		var text = $(this).text();
+		text = text.replace(' ', '');
+		var price = parseInt(text, 10);
+		//console.log(price);
+		var isANumber = (!isNaN(price));
+		if (isANumber) {
+			sum = sum + price;
+			console.log(sum);
+		}
+	});
+});
